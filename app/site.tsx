ddc541c18f02e66fd0d0e16250383d9f6d07b767
Abdul Mueed <amueed121@gmail.com>
2026-09-03 22:36:31 +0500

    new image on main page

--- app/site.tsx
+++ app/site.tsx
@@ -517,7 +517,7 @@ function WorkFeature() {
     <article className="work-feature">
       <div className="work-image">
         <Image
-          src="/asset1.png"
+          src="/asset2.png"
           alt="Eleven Solutions call interface"
           fill
           sizes="(max-width:800px) 90vw, 63vw"
